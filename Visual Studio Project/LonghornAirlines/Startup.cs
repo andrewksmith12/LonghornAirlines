02bea050c75c78d173154114f4a2d9ee385d7a58
andrewksmith12 <43506065+andrewksmith12@users.noreply.github.com>
2020-04-02 13:30:47 -0500

    Add and comment out connection string

--- Visual Studio Project/LonghornAirlines/Startup.cs
+++ Visual Studio Project/LonghornAirlines/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using LonghornAirlines.DAL;
+//using LonghornAirlines.DAL;
 
 namespace LonghornAirlines
 {
@@ -17,9 +17,9 @@ namespace LonghornAirlines
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            String connectionString = "Server=tcp:mis333ksp20.database.windows.net,1433;Initial Catalog=MIS333kSP20-102;Persist Security Info=False;User ID=MIS333kSP20102user;Password=!333Ksp201675;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+            //services.AddMvc();
+            //String connectionString = "Server=tcp:mis333ksp20.database.windows.net,1433;Initial Catalog=MIS333kSP20-102;Persist Security Info=False;User ID=MIS333kSP20102user;Password=!333Ksp201675;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
