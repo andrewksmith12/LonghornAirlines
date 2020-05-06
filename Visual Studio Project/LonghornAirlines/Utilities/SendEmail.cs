484ae510cb975efaa38e2af83297f178955d8b13
danalee101 <60078105+danalee101@users.noreply.github.com>
2020-05-06 16:01:43 -0500

    worked on sendemail

---
+++ Visual Studio Project/LonghornAirlines/Utilities/SendEmail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//TODO: You need to add these using statements to get mail to work
+using System.Net.Mail;
+using System.Net;
+
+namespace LonghornAirlines.Utilites
+{
+    public static class EmailMessaging
+    {
+        public static void SendEmail(String toEmailAddress, String emailSubject, String emailBody)
+        {
+            //Create an email client to send the emails
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                //This is the SENDING email address and password
+                Credentials = new NetworkCredential("dummyemail@gmail.com", "Password123"),
+                EnableSsl = true
+            };
+            //Add anything that you need to the body of the message
+            // /n is a new line – this will add some white space after the main body of the message
+            String finalMessage = emailBody + "\n\n This is a disclaimer that will be on all messages.";
+
+
+            //Create an email address object for the sender address
+            MailAddress senderEmail = new MailAddress("dummyemail@gmail.com", "Bevo's Bookstore");
+            MailMessage mm = new MailMessage();
+            mm.Subject = "Team 6 - " + emailSubject;
+            mm.Sender = senderEmail;
+            mm.From = senderEmail;
+            mm.To.Add(new MailAddress(toEmailAddress));
+            mm.Body = finalMessage;
+            client.Send(mm);
+        }
+    }
+}
