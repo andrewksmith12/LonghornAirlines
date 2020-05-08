77711d2361d77184a92e63f98c210484e9995d41
andrewksmith12 <43506065+andrewksmith12@users.noreply.github.com>
2020-05-07 19:39:22 -0500

    Minor Fix- AddFlights

--- Visual Studio Project/LonghornAirlines/Utilities/AddFlight.cs
+++ Visual Studio Project/LonghornAirlines/Utilities/AddFlight.cs
@@ -14,62 +14,82 @@ namespace LonghornAirlines.Utilities
     {
         public static void addBools(AppDbContext db, FlightInfo flightInfo, Boolean sun, Boolean mon, Boolean tue, Boolean wed, Boolean thurs, Boolean fri, Boolean sat)
         {
+            List<FlightInfo> info = db.FlightInfos.ToList<FlightInfo>();
             List<Flight> AllFlights = new List<Flight>();
-            Boolean[] days = new bool[7];
-            days[0] = sun;
-            days[1] = mon;
-            days[2] = tue;
-            days[3] = wed;
-            days[4] = thurs;
-            days[5] = fri;
-            days[6] = sat;
+                Boolean[] days = new bool[7];
+                days[0] = sun;
+                days[1] = mon;
+                days[2] = tue;
+                days[3] = wed;
+                days[4] = thurs;
+                days[5] = fri;
+                days[6] = sat;
+                Decimal baseFare = flightInfo.BaseFare;
 
-            //For Loop - Goes over all days of the week and add flights to all dates available if true
-            for (int i = 0; i < 7; i++)
-            {
-                if (days[i])
+                //For Loop - Goes over all days of the week and add flights to all dates available if true
+                for (int i = 0; i < 7; i++)
                 {
-                    DateTime startDate = DateTime.Now;
-                    DateTime endDate = Info.END_DATE;
-                    int weekDay = i;
-                    int startWeekDay = Convert.ToInt32(startDate.DayOfWeek);
+                    if (days[i])
+                    {
+                        DateTime startDate = DateTime.Now;
+                        DateTime endDate = Info.END_DATE;
+                        int weekDay = i;
+                        int startWeekDay = Convert.ToInt32(startDate.DayOfWeek);
 
-                    DateTime flightDate = startDate.AddDays(weekDay - startWeekDay);
-                    if (weekDay >= startWeekDay)
-                    {
-                        //Adds the flight for the first week
-                        Flight tempFlight = new Flight
+                        DateTime flightDate = startDate.AddDays(weekDay - startWeekDay);
+                        if (weekDay >= startWeekDay)
                         {
-                            Date = flightDate,
-                            FlightInfo = db.FlightInfos.FirstOrDefault(f => f.FlightNumber == flightInfo.FlightNumber),
-                        };
-                        AllFlights.Add(tempFlight);
-                    }
-                    flightDate = flightDate.AddDays(7);
-                    //Adds flights for all weeks after first week
-                    while (flightDate <= endDate)
-                    {
-                        Flight tempFlight = new Flight
-                        {
-                            Date = flightDate,
-                            FlightInfo = db.FlightInfos.FirstOrDefault(f => f.FlightNumber == flightInfo.FlightNumber),
-                        };
+                            //Adds the flight for the first week
+                            Flight tempFlight = new Flight
+                            {
+                                Date = flightDate,
+                                FlightInfo = db.FlightInfos.FirstOrDefault(f => f.FlightInfoID == flightInfo.FlightInfoID),
+                                BaseFare = baseFare
+                            };
+                            AllFlights.Add(tempFlight);
+                        }
                         flightDate = flightDate.AddDays(7);
-                        AllFlights.Add(tempFlight);
+                        //Adds flights for all weeks after first week
+                        while (flightDate <= endDate)
+                        {
+                            Flight tempFlight = new Flight
+                            {
+                                Date = flightDate,
+                                FlightInfo = db.FlightInfos.FirstOrDefault(f => f.FlightInfoID == flightInfo.FlightInfoID),
+                                BaseFare = baseFare
+                            };
+                            flightDate = flightDate.AddDays(7);
+                            AllFlights.Add(tempFlight);
+                        }
                     }
                 }
+                int intFlightID = db.Flights.Max(m => m.FlightID) + 1;
 
-
-                foreach (Flight seedFlight in AllFlights)
+                try
                 {
+                    foreach (Flight seedFlight in AllFlights)
                     {
-                        //add the genre
-                        db.Flights.Add(seedFlight);
-                        System.Diagnostics.Debug.Write("Flight" + seedFlight + " was created");
-                        db.SaveChanges();
+                        //find the flight in the database
+                        Flight dbFlight = db.Flights.FirstOrDefault(f => f.FlightID == intFlightID);
+
+                        if (dbFlight == null) //the flight isn't in the database
+                        {
+                            //add the genre
+                            db.Flights.Add(seedFlight);
+                            System.Diagnostics.Debug.Write("Flight" + dbFlight + " was created");
+                            db.SaveChanges();
+                        }
+                        intFlightID++;
                     }
                 }
-            }
+                catch (Exception ex)
+                {
+                    StringBuilder msg = new StringBuilder();
+                    msg.Append("There was an error adding the ");
+                    msg.Append(intFlightID);
+                    msg.Append(" flight.");
+                    throw new Exception(msg.ToString(), ex);
+                }
         }
     }
 }
